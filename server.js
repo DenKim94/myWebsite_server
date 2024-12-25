dce78b13707b37d204f6d3816197437b401bfb6a
Denis Kim <denis-kim@outlook.de>
2024-12-25 16:13:27 +0100

    Added logic for user e-mail validation

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ import cors from 'cors';
 
 // Laden und prüfen der Umgebungsvariablen
 dotenv.config();
-const { PORT, URL_CLIENT, CAPTCHA_SECRET_KEY } = process.env;
+const { PORT, URL_CLIENT, CAPTCHA_SECRET_KEY, EMAIL_BLACKLIST } = process.env;
 
 if (!CAPTCHA_SECRET_KEY || !URL_CLIENT) {
   throw new Error('Fehlende Umgebungsvariablen: Bitte setze CAPTCHA_SECRET_KEY und URL_CLIENT.');
 }
+
+// Blacklist aus der .env-Datei in ein Array umwandeln
+const emailBlacklist = EMAIL_BLACKLIST ? EMAIL_BLACKLIST.split(',').map(email => email.trim().toLowerCase()) : [];
+console.log('Blacklist: ', emailBlacklist);
 
 const app = express();
 
@@ -23,12 +27,19 @@ app.use(cors({
 
 app.post('/api/validate-captcha', async (req, res) => {
 
-    // Captcha-Token vom Client erhalten
-    const { captchaToken } = req.body;
+    // Captcha-Token und E-Mail vom Client erhalten
+    const { captchaToken, userEmail } = req.body;
     if (!captchaToken) {
-      return res.status(400).json({ error: 'Ungültiger oder fehlender Captcha-Token.' });
+      return res.status(400).json({ error: 'Ungültiger Captcha-Token.' });
     }
-  
+
+    // Überprüfen, ob die erhaltene E-Mail-Adresse auf der Blacklist steht
+    const isBlockedUser = emailBlacklist.includes(userEmail.toLowerCase());
+
+    if (isBlockedUser) {
+      return res.status(400).json({ error: 'E-Mail-Adresse ist blockiert.' });
+    }
+
     try {
       // Validierung des Captcha-Tokens: Anfrage an die GOOGLE-API
       const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${CAPTCHA_SECRET_KEY}&response=${captchaToken}`;
@@ -44,7 +55,7 @@ app.post('/api/validate-captcha', async (req, res) => {
       if (!verifyResult.success) {
         return res.status(400).json({ error: 'Captcha-Validierung ist fehlgeschlagen.' });
       }
-      res.status(200).json({ message: 'Captcha erfolgreich validiert.' });
+      res.status(200).json({ message: 'Captcha ist erfolgreich validiert.' });
 
     } catch (error) {
       console.error('Serverfehler bei der Captcha-Validierung: ', error);
